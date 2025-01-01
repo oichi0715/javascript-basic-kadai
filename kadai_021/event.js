98b596ce3d9ee7694afb393721f441969237e165
oichi0726 <dorakeika.89@gmail.com>
2025-01-01 13:36:21 +0900

    課題120非同期処理を使用して、２秒後に文章を書き換えよう　アップロード

---
+++ kadai_021/event.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+    setTimeout(() => {
+        text.innerText = 'ボタンをクリックしました'
+    }, 2000);
+})
+
+
+
